c064ebf5a22d6bd6e70705f38b4b17690f52cbe4
Verchai <81279976+Verchas181@users.noreply.github.com>
2024-02-15 14:24:23 -0300

    TADs

--- aula03_TiposDeDados.c
+++ aula03_TiposDeDados.c
@@ -156,5 +156,69 @@ return 0;
 //no .c   struct Array{ int ........};
 
 
+// começo de TAD Matriz
+
+int A[3][3] = {
+  {1,2,3}
+  {4,5,6}
+  {7,8,9}
+}
+
+// necessita de 3 parametros, o valor, e 2 tamanhos
+#ifndef MATRIZ_H
+ #define MATRIZ_H
+
+ typedef struct Matriz Matriz;
+
+ Matriz matriz_create(int rows, int cols);
+ void matriz_destroy(Matriz matriz);
+ void matriz_print(Matriz matriz);
+ int matriz_get(Matriz matriz, int row, int col);
+ void matriz_set(Matriz matriz, int row, int col, int value);
+ int matriz_rows(Matriz matriz);
+ int matriz_cols(Matriz matriz);
+
+ #endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "matriz.h"
+
+struct Matriz {
+ int** array;
+ int rows;
+ int cols;
+ };
+
+ Matriz* matriz_create(int rows, int cols) {
+ Matriz* matriz = malloc(sizeof(Matriz));
+
+ matriz->array = malloc(sizeof(int *) * rows);
+ for (int i = 0; i < rows; i++) {
+ matriz->array[i] = malloc(sizeof(int) * cols);
+ }
+ matriz->rows = rows;
+ matriz->cols = cols;
+
+ return matriz;
+ }
+
+ void matriz_destroy(Matriz* matriz) {
+ for (int i = 0; i < matriz->rows; i++) {
+ free(matriz->array[i]);
+ }
+free(matriz->array);
+ free(matriz);
+ }
+
+ void matriz_print(Matriz* matriz) {
+ for (int i = 0; i < matriz->rows; i++) {
+ for (int j = 0; j < matriz->cols; j++) {
+ printf("%d ", matriz->array[i][j]);
+ }
+ printf("\n");
+ }
+ }
+
 
 
