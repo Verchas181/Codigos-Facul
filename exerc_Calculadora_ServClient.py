9e0ddfec3addeede0f81291091df051c131f2396
Verchas <riquevs18@gmail.com>
2024-02-21 16:34:01 -0300

    Continuar exercicio

--- exerc_Calculadora_ServClient.py
+++ exerc_Calculadora_ServClient.py
@@ -18,8 +18,13 @@ while 1:
     lista = dados.split(' ', 3)
     res = 0
     if lista[0] == "SUM":
-        res = lista[1] + lista[2]
-    if
+        res = int(lista[1]) + int(lista[2])
+    elif lista[0] == "MIN":
+        res = int(lista[1]) - int(lista[2])
+    elif lista[0] == "MUL":
+        res = int(lista[1]) * int(lista[2])
+    elif lista[0] == "DIV":
+        res = int(lista[1]) / int(lista[2])
 
 
 # /////////////////////////////////////#
