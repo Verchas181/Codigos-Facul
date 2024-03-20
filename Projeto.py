6d2f258e67a7dddb5318fc3835e9c5d6a501ac9b
Verchas <riquevs18@gmail.com>
2024-03-20 19:28:50 -0300

    Continuacao do projeto

--- Projeto.py
+++ Projeto.py
@@ -23,49 +23,73 @@ server_socket.listen(1)
 print("Servidor em execução...")
 print("Escutando por conexões na porta %s" % SERVER_PORT)
 
-#cria o while que irá receber as conexões
+
 while True:
-    #espera por conexões
+   
     #client_connection: o socket que será criado para trocar dados com o cliente de forma dedicada
     #client_address: tupla (IP do cliente, Porta do cliente)
     client_connection, client_address = server_socket.accept()
 
     #pega a solicitação do cliente
     request = client_connection.recv(1024).decode()
-    #verifica se a request possui algum conteúdo (pois alguns navegadores ficam periodicamente enviando alguma string vazia)
+    
+
     if request:
+
         #imprime a solicitação do cliente
         print(request)
         
-        #analisa a solicitação HTTP
-        headers = request.split("\n")
-        #print(headers)#impressão dos cabeçalhos
-        #pega o nome do arquivo sendo solicitado
-        filename = headers[0].split()[1]
-
-        #verifica qual arquivo está sendo solicitado e envia a resposta para o cliente
-        if filename == "/":
-            filename = "/index.html"
-
-        #try e except para tratamento de erro quando um arquivo solicitado não existir
-        try:
-            #abrir o arquivo e enviar para o cliente
-            fin = open("htdocs" + filename)
-            #leio o conteúdo do arquivo para uma variável
-            content = fin.read()
-            #fecho o arquivo
-            fin.close()
-            #envia a resposta
-            response = "HTTP/1.1 200 OK\n\n" + content
-        except FileNotFoundError:
-            #caso o arquivo solicitado não exista no servidor, gera uma resposta de erro
-            response = "HTTP/1.1 404 NOT FOUND\n\n<h1>ERROR 404!<br>File Not Found!</h1>"
+       
+        headers = request.split("\n")  #analisa a solicitação HTTP
+        print(headers)
+        filename = headers[0].split()[1]  # nome do arquivo
+        solicitacao = headers[0].split()[0] #tipo de solicitacao GET, PUT ....
 
 
-        #envia a resposta HTTP
-        client_connection.sendall(response.encode())
+        if solicitacao == "PUT":
+            conteudo = headers[5]
+            try:
 
-        client_connection.close()
+                f = open("htdocs" + caminho, "w")
+
+                f.write(conteudo)
+
+                f.close()
+
+                response = "HTTP/1.1 200 OK \n\n"
+            except:
+                print("Erro ao escrever/criar o arquivo")
+                response = "HTTP/1.1 500 Internal Server Error\n\n"
+
+
+
+
+        if solicitacao == "GET":
+            if filename == "/": #verifica qual arquivo está sendo solicitado
+                filename = "/index.html" 
+
+            #try e except para tratamento de erro quando um arquivo solicitado não existir
+            try:
+            
+                fin = open("htdocs" + filename) #abrir o arquivo e
+            
+                content = fin.read() #leio o conteúdo do arquivo 
+            
+                fin.close() #fecho o arquivo
+            
+                response = "HTTP/1.1 200 OK\n\n" + content #envia a resposta + arquivo
+
+
+            except FileNotFoundError:
+                #caso o arquivo solicitado não exista no servidor, gera uma resposta de erro
+                response = "HTTP/1.1 404 NOT FOUND\n\n<h1>ERROR 404!<br>File Not Found!</h1>"
+
+
+            #envia a resposta HTTP
+            client_connection.sendall(response.encode())
+
+            client_connection.close()
+
 
 server_socket.close()
 
