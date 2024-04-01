35333b39642414c1369c53bf62b9f1e626e92e34
Verchas <riquevs18@gmail.com>
2024-04-01 10:57:45 -0300

    Projeto Funcionando

--- Projeto.py
+++ Projeto.py
@@ -28,32 +28,36 @@ while True:
    
     #client_connection: o socket que será criado para trocar dados com o cliente de forma dedicada
     #client_address: tupla (IP do cliente, Porta do cliente)
+
     client_connection, client_address = server_socket.accept()
 
     #pega a solicitação do cliente
-    request = client_connection.recv(8192).decode('latin-1')
+    request = client_connection.recv(8192).decode()
     
 
     if request:
 
         #imprime a solicitação do cliente
-        print(request)
         
-       
         headers = request.split("\n")  #analisa a solicitação HTTP
         print(headers)
         filename = headers[0].split()[1]  # nome do arquivo
-        
         solicitacao = headers[0].split()[0] #tipo de solicitacao GET, PUT ....
 
-        if solicitacao.startswith("PUT /"):
+        print(solicitacao)
+        response = "HTTP/1.1 404 NOT FOUND\n\n<h1>ERRO</h1>"
+
+        
+
+        if solicitacao == "PUT":
             conteudo = headers[5]
             try:
-
-                f = open("htdocs" + caminho, "w")
+                if filename == "/":
+                    f = open("htdocs/padrao.txt","w")
+                else:
+                    f = open("htdocs" + filename, "w")
 
                 f.write(conteudo)
-
                 f.close()
 
                 response = "HTTP/1.1 200 OK \n\n"
@@ -64,16 +68,19 @@ while True:
 
 
 
-        if solicitacao.startswith("GET /"):
+        if solicitacao == "GET":
             if filename == "/": #verifica qual arquivo está sendo solicitado
                 filename = "/index.html" 
 
             #try e except para tratamento de erro quando um arquivo solicitado não existir
             try:
             
-                fin = open("htdocs" + filename) #abrir o arquivo e
+                fin = open("htdocs" + filename) #abrir o arquivo 
+                
             
                 content = fin.read() #leio o conteúdo do arquivo 
+
+                print(content)
                 
             
                 fin.close() #fecho o arquivo
@@ -87,9 +94,9 @@ while True:
 
 
             #envia a resposta HTTP
-client_connection.sendall(response.encode())
+        client_connection.sendall(response.encode())
 
-client_connection.close()
+        client_connection.close()
 
 
 server_socket.close()
