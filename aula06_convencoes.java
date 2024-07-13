a36708a5f771ac3b5648e35cf412a6a5b31230b4
Verchai <81279976+Verchas181@users.noreply.github.com>
2024-07-12 23:53:28 -0300

    Convenções

---
+++ aula06_convencoes.java
@@ -0,0 +1,42 @@
+/*
+
+ _____                                                
+/  __ \                                               
+| /  \/ ___  _ ____   _____ _ __   ___ ___   ___  ___ 
+| |    / _ \| '_ \ \ / / _ \ '_ \ / __/ _ \ / _ \/ __|
+| \__/\ (_) | | | \ V /  __/ | | | (_| (_) |  __/\__ \
+ \____/\___/|_| |_|\_/ \___|_| |_|\___\___/ \___||___/
+                                                      
+                                                      
+
+*/
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  //Um pacote é uma maneira de organizar logicamente um conjunto de classes.
+// A declaração de um pacote é feita de trás para frente, além disso o diretório que contém o pacote deve ter o mesmo nome que o pacote.
+  
+//Por exemplo: 
+package br.edu.ufabc.naming;
+
+// Outras convenções
+
+// Nomes de variáveis: começa com minúscula, em CamelCase
+   
+String iAmAString;
+
+// Nomes de constantes: todas letras maiúsculas,
+// Palavras concatenadas com sublinhado
+
+final int SOME_CONSTANT = 10;
+
+// Nomes dos métodos: iniciando em minúscula, em CamelCase
+
+public void methodThatDoesSomething(int someValue) {
+
+// Importação de pacotes
+  
+import banco.ContaCorrente;
+
+// Estas são convenções básicas, porém há outras convenções que são usadas, podendo ser verificadas em diversos guias de convenções.
+
+    
