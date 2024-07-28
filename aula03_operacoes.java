e50529f1df9b5d776bfa15f43b29a57bf3cc586f
Verchai <81279976+Verchas181@users.noreply.github.com>
2024-07-28 20:08:30 -0300

    Operações

--- aula03_operacoes.java
+++ aula03_operacoes.java
@@ -120,7 +120,28 @@ switch (dia) {
       System.out.printf("Soma deu %d",c);
       }
 }
+
+--------------------------------------------------------------------------------------------------------------------------------------------------
       
+--- Recursividade ---
+
+
+
+  public class Recursividade {
+    public static void main(String[] args) {
+        int n = 5;
+        int fatorial = fatorial(n);
+        System.out.printf("O fatorial de %d é %d\n", n, fatorial);
+    }
+
+    public static int fatorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * fatorial(n - 1);
+        }
+    }
+}
 
 
   
