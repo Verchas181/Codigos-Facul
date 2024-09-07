f70589b353125129592b58bd735aaa33c6f73ada
Verchai <81279976+Verchas181@users.noreply.github.com>
2024-09-07 14:43:50 -0300

    Pilha, Fila, Array Dinâmico

--- aula20_AED&POO.java
+++ aula20_AED&POO.java
@@ -129,25 +129,202 @@ xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  // Pilha
+  // Fila
 
   // FIFO (First in first out)
 
 
   package queue;
 
-  public interface Queue{
+import exceptions.StructureEmptyException;
+import exceptions.StructureFullException;
 
-    voi enqueue(int value);
-    int dequeue();
-    int front();
-    int rear();
+public interface Queue {
+    void enqueue(int value) throws StructureFullException;
+    int dequeue() throws StructureEmptyException;
+    int front() throws StructureEmptyException;
+    int rear() throws StructureEmptyException;
     int size();
     int capacity();
     boolean isEmpty();
     boolean isFull();
-  }
+}
 
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  // Fila Circular
+
+  public class CircularQueue {
+    private int size;
+    private int[] elements;
+    private int front;
+    private int back;
+
+    public CircularQueue(int capacity) throws InvalidCapacityException {
+        if (capacity <= 0) {
+            throw new InvalidCapacityException(capacity);
+        }
+        this.size = 0;
+        this.elements = new int[capacity];
+        this.front = 0;
+        this.back = 0;
+    }
+
+     public int size() {
+     return size;
+ }
+ 
+ public boolean isEmpty() {
+     return size == 0;
+ }
+ 
+ public boolean isFull() {
+     return size == capacity();
+ }
+ 
+ public int capacity() {
+     return elements.length;
+ }
+ 
+ public int front() throws StructureEmptyException {
+     if (isEmpty()) {
+         throw new StructureEmptyException(capacity(), size());
+     }
+     return elements[front];
+ }
+ 
+ public int rear() throws StructureEmptyException {
+     if (isEmpty()) {
+         throw new StructureEmptyException(capacity(), size);
+     }
+     return elements[(back - 1 + capacity()) % capacity()];
+ }
+
+  public void enqueue(int element) throws StructureFullException {
+    if (isFull()) {
+        throw new StructureFullException(capacity(), size);
+    }
+    elements[back] = element;
+    back = (back + 1) % capacity();
+    size++;
+}
+
+public int dequeue() throws StructureEmptyException {
+    if (isEmpty()) {
+        throw new StructureEmptyException(capacity(), size);
+    }
+    int element = elements[front];
+    front = (front + 1) % capacity();
+    size--;
+    return element;
+}
+
+public void clear() {
+    front = 0;
+    back = 0;
+    size = 0;
+}
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ // Array Dinâmico
+
+ public class DynamicArray {
+    private int[] elements;
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public DynamicArray() {
+        elements = new int[DEFAULT_CAPACITY];
+    }
+
+    public DynamicArray(int capacity) throws InvalidCapacityException {
+        if (capacity <= 0) {
+            throw new InvalidCapacityException(capacity);
+        }
+        elements = new int[capacity];
+    }
+}
+
+ public int size() { 
+    return size; 
+}
+
+public boolean isEmpty() { 
+    return size == 0; 
+}
+
+public int capacity() {
+    return elements.length;
+}
+
+ private void validateIndex(int index) throws InvalidIndexException {
+    if (index < 0 || index >= size) {
+        throw new InvalidIndexException(index, size);
+    }
+}
+
+public int get(int index) throws InvalidIndexException {
+    validateIndex(index);
+    return elements[index];
+}
+
+public void set(int index, int element) throws InvalidIndexException {
+    validateIndex(index);
+    elements[index] = element;
+}
+
+private void resize() {
+    int[] newElements = new int[elements.length * 2];
+    for (int i = 0; i < size; i++) {
+        newElements[i] = elements[i];
+    }
+    elements = newElements;
+}
+
+private void shiftRight(int index) {
+    for (int i = size; i > index; i--) {
+        elements[i] = elements[i - 1];
+    }
+}
+
+public void insert(int index, int element) throws InvalidIndexException {
+    validateIndex(index);
+    if (size == elements.length) {
+        resize();
+    }
+    shiftRight(index);
+    elements[index] = element;
+    size++;
+}
+
+private void shiftLeft(int index) {
+    for (int i = index; i < size - 1; i++) {
+        elements[i] = elements[i + 1];
+    }
+}
+
+public int remove(int index) throws InvalidIndexException {
+    validateIndex(index);
+    int element = elements[index];
+    shiftLeft(index);
+    size--;
+    return element;
+}
+
+public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("DynamicArray: [");
+    if (!isEmpty()) {
+        sb.append(elements[0]);
+        for (int i = 1; i < size; i++) {
+            sb.append(", ");
+            sb.append(elements[i]);
+        }
+    }
+    sb.append("]");
+    return sb.toString();
+}
 
 
   
